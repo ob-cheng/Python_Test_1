3f1e8a4c4b3a7fbff6dc98b7db85d3fe5806e65f
ob-cheng <92861361+ob-cheng@users.noreply.github.com>
2021-12-08 12:34:41 +0100

    idk

---
+++ conditions.py
@@ -0,0 +1,39 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+# 
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+p = True
+q = False
+
+if p and q:
+    print ('p and q')
+else:
+    print('not right')
+
+if p or q:
+    print ('p or q')
+else:
+    print('not right')
+
+
+x = 19
+p= x<16
+if p:
+    print('oh yeah')
+else:
+    print('nope')
