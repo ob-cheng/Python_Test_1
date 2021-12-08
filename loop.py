de2bc29f48e29a960e3716876928bd68208c2a2c
ob-cheng <92861361+ob-cheng@users.noreply.github.com>
2021-12-08 15:10:04 +0100

    how to do loops

--- loop.py
+++ loop.py
@@ -1,5 +1,5 @@
 import it as it
-
+# 'for' loops
 for i in range(8):
     print(i)
 
@@ -14,3 +14,20 @@ for i in range(3,8):
     if i==6:
         break
     print(i)
+
+print('------------------')
+
+x=10
+for i in range(1,x+1):
+    print(i)
+
+# 'while' loop
+print('-----------')
+
+a=10
+for i in range(3):
+    print(i)
+    print(a)
+    b=10
+print('a=%s'%a)
+print('b=%s'%b)
