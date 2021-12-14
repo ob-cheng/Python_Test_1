efe75d948e0311db0f515c0393fc60c5da6773dd
ob-cheng <92861361+ob-cheng@users.noreply.github.com>
2021-12-14 13:43:28 +0100

    word count hw

---
+++ Word Counting.py
@@ -0,0 +1,17 @@
+import string
+text = open("word_count.txt", "r")
+dic = dict()
+for line in text:
+
+    line = line.strip()
+    line = line.lower()
+    line = line.translate(line.maketrans("", "", string.punctuation))
+    words = line.split(" ")
+    for word in words:
+        if word in dic:
+            dic[word] = dic[word] + 1
+        else:
+            dic[word] = 1
+
+for key in list(dic.keys()):
+    print(key, ":", dic[key])
